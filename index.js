0588dd54ae815ed10e8e1ea491276b6b0cf1b76d
Matt Boggs <matt.david.boggs@gmail.com>
2020-10-14 15:43:59 -0600

    title, description, and TOC made

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
-var fs = require("fs");
-var inquirer = require("inquirer");
-var generateMarkdown = require("./util/generateMarkdown")
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./util/generateMarkdown")
 
-// array of questions for user
-const questions = [
+function makeTitle(){
+    inquirer.prompt({
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    }).then(function(response){
+        fs.writeFile("sample.md", "# " + response.title + "\n", function(err){
+            if (err){
+                return console.log(err);
+            };
+            makeDescription();
 
-];
+        });
+    });
+
+};
+function makeDescription(){
+    inquirer.prompt({
+        type: "input",
+        message: "write a brief description of your project here",
+        name: "description"
+    }).then(function(response){
+        fs.appendFile("sample.md", "\n## Info: \n" + response.description + "\n", function(err){
+            if (err){
+                return console.log(err);
+            };
+            makeTOC();
+        });
+    });
+};
+function makeTOC(){ // 
+    fs.appendFile("sample.md","\n## Table of Contents \n Installation \n Usage \n License \n Contributing \n Tests \n Questions", function(err){
+        if (err){
+            return console.log(err);
+        }
+    })
+};
+
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// fs.appendFile("sample.md", data, function(err){
+//     if (err){
+//         return console.log(err);
+//     }
+// });
 
 // function to initialize program
 function init() {
-
+    makeTitle();
 }
 
 // function call to initialize program
 init();
+
+
+// * Title
+//   * Description
+//   * Table of Contents
+//   * Installation
+//   * Usage
+//   * License
+//   * Contributing
+//   * Tests
+//   * Questions
